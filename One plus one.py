c6b0724d9a0964394c42d2fb818d099ab0ab5720
richard-fung-db <richard-fung-db>
2022-11-30 22:46:06 +0000

    remove text

--- One plus one.py
+++ One plus one.py
@@ -16,4 +16,11 @@
 # COMMAND ----------
 
 sql("select * from default.diamonds limit 10")
+<<<<<<< HEAD
 >>>>>>> d42ab21 (Merge conflict commit)
+=======
+
+# COMMAND ----------
+
+
+>>>>>>> 1c57851 (remove text)
